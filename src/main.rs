db7a42767aa406dbaad3f8afbd0fc95a32a0ce81
Leonardo Galer <connect@leonardogaler.com>
2023-05-05 18:36:39 -0600

    change api key

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use wasm_bindgen::{JsCast, UnwrapThrowExt};
 use web_sys::{Event, HtmlInputElement, InputEvent};
 use yew::{function_component, html, use_state, Callback, Properties, UseStateHandle};
-use crate::youtube::search_youtube;
+use youtube::search_youtube;
 
+mod env;
 mod youtube;
 
 fn main() {
-   yew::start_app::<App>();
+    yew::start_app::<App>();
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Video {
     id: String,
     name: String,
@@ -17,74 +18,88 @@ struct Video {
 
 #[function_component(App)]
 fn app() -> Html {
-    let video :UseStateHandle<Option<Video>> = use_state(|| None);
+    let video: UseStateHandle<Option<Video>> = use_state(|| None);
+
     let on_search = {
         let video = video.clone();
-        Callback::from(move |text_to_search| {
-        let video_id = search_youtube(text_to_search);
-        video.set(Some(Video{
-            id: video_id,
-            name: "name".to_string()
-        }))
+        Callback::from(move |text_to_search: String| {
+            let video = video.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                match search_youtube(text_to_search).await {
+                    Ok(video_item) => video.set(Some(Video {
+                        id: video_item.id.video_id,
+                        name: video_item.snippet.title,
+                    })),
+                    Err(e) => {
+                        web_sys::console::log_1(&e.to_string().into());
+                    }
+                }
+            });
         })
     };
 
-    let video_selection = match (*video).clone(){
-        Some(video) => html!{
-            <VideoPlayer video_title={video.name} video_id={video.id}/>
+    let video_section = match (*video).clone() {
+        Some(video) => html! {
+            <VideoSection name={video.name} id={video.id} />
         },
-        None => html!{}
+        None => html! {},
     };
+
     html! {
         <main>
-        <VideoControls on_search={on_search}/>
-        {video_selection}
-        <VideoPlayer video_title="{video.unwrap().name}" video_id="{video.unwrap().id}"/>
+        <VideoControls on_search={on_search} />
+        {video_section}
         </main>
     }
 }
 
 #[derive(Properties, PartialEq)]
-struct VideoControlsProps {
+struct VideoControlProps {
     on_search: Callback<String>,
 }
 
 #[function_component(VideoControls)]
-fn video_controls(props: &VideoControlsProps) -> Html {
+fn controls(props: &VideoControlProps) -> Html {
     let text_to_search = use_state(|| String::new());
+
     let handle_input = {
         let text_to_search = text_to_search.clone();
         Callback::from(move |input_event| {
-        let text = get_value_from_input_event(input_event);
-        text_to_search.set(text);
+            let text = get_value_from_input_event(input_event);
+            text_to_search.set(text);
         })
     };
-    
-    let on_search_pressed = { 
+
+    let on_search_pressed = {
         let on_search = props.on_search.clone();
         Callback::from(move |_| on_search.emit(text_to_search.to_string()))
     };
+
     html! {
         <div>
-            <h1>{ "Search a word" }</h1>
-            <input type="text" oninput={handle_input}/>
-            <div><button onclick={on_search_pressed}>{"SEARCH"}</button></div>
+            <div>
+                {"Ingresa una palabra"}
+            </div>
+            <div>
+                <input type="text" oninput={handle_input} />
+            </div>
+            <div><button onclick={on_search_pressed}>{"buscar!"}</button></div>
         </div>
     }
 }
 
 #[derive(Properties, PartialEq)]
-struct VideoSectionProps{
-    video_id: String,
-    video_title: String,
+struct VideoSectionProps {
+    id: String,
+    name: String,
 }
 
-#[function_component(VideoPlayer)]
-fn video_player(props: &VideoSectionProps) -> Html {
-    let yt_url = format!("https://www.youtube.com/embed/{}", props.video_id);
+#[function_component(VideoSection)]
+fn video_section(props: &VideoSectionProps) -> Html {
+    let yt_url = format!("https://www.youtube.com/embed/{}", props.id);
     html! {
         <div>
-            <iframe with="1920" height="1080" src={yt_url}></iframe>
+            <iframe width="560" height="315" src={yt_url}></iframe>
         </div>
     }
 }
